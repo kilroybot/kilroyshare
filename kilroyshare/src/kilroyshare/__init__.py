8ac0f07893fa2f5059400d4430213784140cf058
Sebastian Pietras <01133337@pw.edu.pl>
2022-02-25 00:57:31 +0100

    Cleaned up (#6)
    * Cleaned up README
    
    * Cleaned up .gitignore
    
    * Applied formatting
    
    * Added poetry dependencies

--- kilroyshare/src/kilroyshare/__init__.py
+++ kilroyshare/src/kilroyshare/__init__.py
@@ -3,23 +3,20 @@ from pathlib import Path, PurePath
 from typing import ContextManager, Tuple, Union
 
 from kilroyshare import resources
+
 #
 # Exposing things, so they can be imported by `kilroyshare.<thing>`
 #
-from kilroyshare.interfaces import (
-    KilroyFace
-)
-from kilroyshare.post import (
-    PostData
-)
+from kilroyshare.interfaces import KilroyFace
+from kilroyshare.post import PostData
 
 
 def extract_resource_path(
-        resource_path: Union[str, PurePath]
+    resource_path: Union[str, PurePath]
 ) -> Tuple[str, str]:
     """Returns path to resource as tuple with parent part (as package with dots) and resource name."""
     resource_path = PurePath(resource_path)
-    if not resource_path.is_relative_to(PurePath('.')):
+    if not resource_path.is_relative_to(PurePath(".")):
         raise ValueError(
             f"Path has to be relative to current dir, "
             f"but '{resource_path}' is not"
@@ -31,28 +28,25 @@ def extract_resource_path(
     return ".".join(resource_path.parent.parts), resource_path.name
 
 
-def resource(
-        resource_path: Union[str, PurePath]
-) -> ContextManager[Path]:
+def resource(resource_path: Union[str, PurePath]) -> ContextManager[Path]:
     """Wrapper to importlib.resources.path with package part filled."""
     package_part, resource_name = extract_resource_path(resource_path)
-    package_part = f"{resources.__name__}" \
-                   f"{'.' + package_part if package_part else ''}"
+    package_part = (
+        f"{resources.__name__}" f"{'.' + package_part if package_part else ''}"
+    )
     return path(package_part, resource_name)
 
 
-def resource_bytes(
-        resource_path: Union[str, PurePath]
-) -> bytes:
+def resource_bytes(resource_path: Union[str, PurePath]) -> bytes:
     """Wrapper to Path.read_bytes() that uses resource(resource_path)."""
     with resource(resource_path) as r:
         return r.read_bytes()
 
 
 def resource_text(
-        resource_path: Union[str, PurePath],
-        encoding: str = None,
-        errors: str = None
+    resource_path: Union[str, PurePath],
+    encoding: str = None,
+    errors: str = None,
 ) -> str:
     """Wrapper to Path.read_text() that uses resource(resource_path)."""
     with resource(resource_path) as r:
